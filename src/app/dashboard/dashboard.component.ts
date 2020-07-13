ab4cb2e641f81c4da6048d056fc6f959d3b12757
aniketsbdev <aniketsbdev@gmail.com>
2020-07-13 17:36:05 +0530

    Basic Logic Set

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  displayArray: any[] = [];
+  department: any = {
+    manager: [{ developer: [], qaTester: [], manager: [] }]
+  };
+  developer: any = {
+    name: "Developer",
+    cost: 1000
+  };
+  qaTester: any = {
+    name: "QA Tester",
+    cost: 500
+  };
+  manager: any = {
+    name: "Manager",
+    cost: 300
+  };
+  finalDepartments: any[] = [this.department];
+  departmentForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    // this.departmentForm = this.formBuilder.group({
+    //   departmentNumber: [],
+    //   managers: this.formBuilder.array([this.addManagerGroup()])
+    // });
   }
 
+  ngOnInit() {
+    this.displayArray.push(this.finalDepartments);
+  }
+
+  // private addManagerGroup(): FormGroup {
+  //   return this.formBuilder.group({
+  //     manager: [],
+  //     developers: [],
+  //     state: [],
+  //     contacts: this.formBuilder.array([])
+  //   });
+  // }
+
+  addDept(departmentNumber: any) {
+    this.finalDepartments.push(this.department);
+  }
 }
