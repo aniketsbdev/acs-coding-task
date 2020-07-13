f67327a9d0f5b5b263765349a7899458a0b40ef2
aniketsbdev <aniketsbdev@gmail.com>
2020-07-14 01:46:29 +0530

    View Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { IntroductionComponent } from "./introduction/introduction.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { MatCardModule } from "@angular/material/card";
-import {MatBadgeModule} from '@angular/material/badge';
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, MatCardModule,MatBadgeModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatBadgeModule
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
